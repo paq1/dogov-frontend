7d90260f5bcc5273aed1c65d093b993116d652a5
Pierre Paquin <p.paquin1016@laposte.net>
2024-07-27 06:22:08 +0200

    Initial commit

---
+++ src/pages/About.tsx
@@ -0,0 +1,18 @@
+
+import Navigation from "../components/Navigation";
+
+
+const About = () => {
+    return (
+        <div>
+            <Navigation />
+            <h1>ABOUT</h1>
+            <br/>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus doloremque, minima nemo sequi similique sit vitae! Accusamus aliquid, dignissimos dolorum esse ipsa ipsum modi necessitatibus quam similique tempore totam unde!</p>
+            <br/>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad cum deleniti dolorem, doloribus eos illum incidunt magni modi nesciunt quas quibusdam reiciendis repellendus sed tempora ullam. Animi architecto asperiores culpa ea enim explicabo, nam neque porro quisquam rerum temporibus vel. Accusantium adipisci, amet assumenda dolor enim laborum laudantium minus nostrum, omnis, perspiciatis quam quia quibusdam unde velit voluptate! Aliquam aperiam autem consectetur cum debitis dicta distinctio dolor, dolores ducimus eaque enim et excepturi facilis fugit hic illo ipsum libero nobis officia optio quasi quis ratione rem rerum sapiente unde vero. Blanditiis debitis illum iure, laborum magni molestias numquam provident quos!</p>
+        </div>
+    )
+}
+
+export default About;
---
+++ src/pages/Home.tsx
@@ -0,0 +1,12 @@
+import Navigation from "../components/Navigation";
+
+function Home() {
+
+    return (
+        <div>
+            <Navigation />
+            <h1>ACCUEIL</h1>
+        </div>
+    )
+}
+export default Home;
